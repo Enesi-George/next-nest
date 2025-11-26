61459404961216ed7b33ec12922a7027a337fc55
georgeenesi81@gmail.com <georgeenesi81@gmail.com>
2025-11-26 13:31:23 +0100

    enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+    app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://next-nest-production-9092.up.railway.app/',
+    ],
+    credentials: true,
+  });
   
   await app.listen(3001);
 }
